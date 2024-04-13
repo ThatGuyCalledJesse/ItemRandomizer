762efc5dd629ba0d451509c7fcf1f976b18c40e5
Jesse Swart <jesseswart08@gmail.com>
2024-04-13 18:29:16 +0200

    Finished a listener and included author in plugin.yml

--- src/main/java/com/jesse/itemrandomizer/listeners/InventoryOpenListener.java
+++ src/main/java/com/jesse/itemrandomizer/listeners/InventoryOpenListener.java
@@ -5,14 +5,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.AbstractHorseInventory;
 
 public class InventoryOpenListener implements Listener {
     @EventHandler
     public static void onInventoryOpen(InventoryOpenEvent event) {
         // TODO: Add all storage blocks
-        if (event.getInventory().getType().equals(InventoryType.CHEST) || event.getInventory().getType().equals(InventoryType.ENDER_CHEST) || event.getInventory().getType().equals(InventoryType.BARREL) || event.getInventory().getType().equals(InventoryType.DISPENSER) || event.getInventory().getType().equals(InventoryType.DROPPER) || event.getInventory().getType().equals(InventoryType.SHULKER_BOX)) {
-            event.setCancelled(true);
-            event.getPlayer().sendMessage(ChatColor.RED + "No chests or other storage items! It says so in the rules...");
+        if (event.getInventory().getType().equals(InventoryType.CHEST) || event.getInventory().getType().equals(InventoryType.ENDER_CHEST) || event.getInventory().getType().equals(InventoryType.BARREL) || event.getInventory().getType().equals(InventoryType.DISPENSER) || event.getInventory().getType().equals(InventoryType.DROPPER) || event.getInventory().getType().equals(InventoryType.SHULKER_BOX) || event.getInventory().getType().equals(InventoryType.HOPPER)) {
+            if (!(event.getInventory() instanceof AbstractHorseInventory)) {
+                event.setCancelled(true);
+                event.getPlayer().sendMessage(ChatColor.RED + "No chests or other storage items! It says so in the rules...");
+            }
         }
     }
 }
