4aeaf6f2873f60a998eb306e7e2fe216753d07fd
Jesse Swart <jesseswart08@gmail.com>
2024-04-13 17:10:37 +0200

    Wrote the listener

--- src/main/java/com/jesse/itemrandomizer/listeners/InventoryOpenListener.java
+++ src/main/java/com/jesse/itemrandomizer/listeners/InventoryOpenListener.java
@@ -1,4 +1,18 @@
 package com.jesse.itemrandomizer.listeners;
 
-public class InventoryOpenListener {
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
+
+public class InventoryOpenListener implements Listener {
+    @EventHandler
+    public static void onInventoryOpen(InventoryOpenEvent event) {
+        // TODO: Add all storage blocks
+        if (event.getInventory().getType().equals(InventoryType.CHEST) || event.getInventory().getType().equals(InventoryType.ENDER_CHEST) || event.getInventory().getType().equals(InventoryType.BARREL) || event.getInventory().getType().equals(InventoryType.DISPENSER) || event.getInventory().getType().equals(InventoryType.DROPPER) || event.getInventory().getType().equals(InventoryType.SHULKER_BOX)) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.RED + "No chests or other storage items! It says so in the rules...");
+        }
+    }
 }
