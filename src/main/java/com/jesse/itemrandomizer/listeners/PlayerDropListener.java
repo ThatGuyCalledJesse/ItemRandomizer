9e9d8934b2c1c307413178e1868900a2c2418f68
Jesse Swart <jesseswart08@gmail.com>
2024-04-13 17:13:37 +0200

    Wrote the listener

--- src/main/java/com/jesse/itemrandomizer/listeners/PlayerDropListener.java
+++ src/main/java/com/jesse/itemrandomizer/listeners/PlayerDropListener.java
@@ -1,4 +1,12 @@
 package com.jesse.itemrandomizer.listeners;
 
-public class PlayerDropListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+public class PlayerDropListener implements Listener {
+    @EventHandler
+    public static void onPlayerItemDrop(PlayerDropItemEvent event) {
+        event.setCancelled(true);
+    }
 }
