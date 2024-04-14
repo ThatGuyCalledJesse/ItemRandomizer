ecbdf014917b61f67ea59f16a2bd1c35944726c6
Jesse Swart <jesseswart08@gmail.com>
2024-04-14 08:22:41 +0200

    Wrote the player join listener

--- src/main/java/com/jesse/itemrandomizer/listeners/PlayerJoinListener.java
+++ src/main/java/com/jesse/itemrandomizer/listeners/PlayerJoinListener.java
@@ -1,4 +1,11 @@
 package com.jesse.itemrandomizer.listeners;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
 public class PlayerJoinListener {
+    @EventHandler
+    public static void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getDisplayName() + ". Use /rules to see the rules for this challenge.");
+    }
 }
